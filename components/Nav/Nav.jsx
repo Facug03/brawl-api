98e50801374787c8b05e7fcd0cb51580b573e9d0
Facug03 <facundogonzales1550@gmail.com>
2023-01-04 23:25:30 -0300

    text center and div changed to ul

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -3,12 +3,14 @@ import styles from './Nav.module.css'
 export default function Nav() {
   return (
     <header className={styles.header}>
-      <div className={styles.game}>BrawlPro</div>
-      <div className={styles.nav}>
-        <div>Eventos</div>
-        <div>Rankings</div>
-        <div>Clubs</div>
-      </div>
+      <h1 className={styles.game}>
+        Brawl<span className={styles.pro}>Pro</span>
+      </h1>
+      <ul className={styles.nav}>
+        <li>Eventos</li>
+        <li>Rankings</li>
+        <li>Clubs</li>
+      </ul>
     </header>
   )
 }
