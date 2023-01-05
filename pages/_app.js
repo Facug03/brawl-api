12462302d57974f2a12d812e4122105212d3b6f3
Facug03 <facundogonzales1550@gmail.com>
2023-01-04 23:25:55 -0300

    Nav added in app to appear in all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { Rubik } from '@next/font/google'
 
+import Nav from '../components/Nav/Nav'
 import '../styles/globals.css'
 
 const rubik = Rubik({ subsets: ['latin'] })
@@ -7,6 +8,7 @@ const rubik = Rubik({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <div className={rubik.className}>
+      <Nav />
       <Component {...pageProps} />
     </div>
   )
