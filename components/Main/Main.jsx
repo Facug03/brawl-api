613ffffe25e27cd766a2ec776c404a7b3ad4ed27
Facug03 <facundogonzales1550@gmail.com>
2023-01-04 23:25:04 -0300

    drop menu and styles improved

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState } from 'react'
 
 import styles from './Main.module.css'
+import whereIs from '../../public/whereisprofile.jpg'
+import profile from '../../public/profile.jpg'
 
 export default function Main() {
   const [idPlayer, setIdPlayer] = useState('')
   const [idClub, setIdClub] = useState('')
+  const [dropPlayer, setDropPlayer] = useState(false)
+  const [dropClub, setDropClub] = useState(false)
 
   const controlInput = ({ target }) => {
     console.log(target.value)
@@ -26,36 +31,119 @@ export default function Main() {
 
   return (
     <main className={styles.main}>
-      <div className={`${styles.card} ${styles.player}`}>
-        <h2 className={styles.search}>Search player by tag</h2>
-        <form className={styles.form}>
-          <span className={styles.hashtag}>#</span>
-          <input
-            value={idPlayer}
-            name='player'
-            onChange={controlInput}
-            className={styles.tag}
-          />
-          <Link href={`/player/${idPlayer}`} className={styles.button}>
-            Search
-          </Link>
-        </form>
-      </div>
-      <div className={styles.card}>
-        <h2 className={styles.search}>Search club by tag</h2>
-        <form className={styles.form}>
-          <span className={styles.hashtag}>#</span>
-          <input
-            value={idClub}
-            name='club'
-            onChange={controlInput}
-            className={styles.tag}
-          />
-          <Link href='/' className={styles.button}>
-            Search
-          </Link>
-        </form>
-      </div>
+      <h2 className={styles.title}>
+        Search your profile or club <span className={styles.stats}>stats</span>!
+      </h2>
+      <section className={styles.section}>
+        <article className={styles.card}>
+          <h2 className={styles.search}>Player</h2>
+          <form className={styles.form}>
+            <span className={styles.hashtag}>#</span>
+            <input
+              value={idPlayer}
+              name='player'
+              onChange={controlInput}
+              className={styles.tag}
+            />
+            <Link href={`/player/${idPlayer}`} className={styles.button}>
+              Search
+            </Link>
+          </form>
+        </article>
+        <article className={styles.card}>
+          <h2 className={styles.search}>Club</h2>
+          <form className={styles.form}>
+            <span className={styles.hashtag}>#</span>
+            <input
+              value={idClub}
+              name='club'
+              onChange={controlInput}
+              className={styles.tag}
+            />
+            <Link href='/' className={styles.button}>
+              <p className={styles.link}>Search</p>
+            </Link>
+          </form>
+        </article>
+      </section>
+      <section>
+        <div onClick={() => setDropPlayer(!dropPlayer)} className={styles.drop}>
+          <h3 className={styles.text}>
+            <div className={`${dropPlayer && styles.open} ${styles.close}`}>
+              {'>'}
+            </div>{' '}
+            Where is my Player ID?
+          </h3>
+        </div>
+        {dropPlayer && (
+          <article className={styles.tutorial}>
+            <div className={styles.div}>
+              <p className={styles.p}>
+                Enter to your profile <span className={styles.stats}>here</span>
+              </p>
+              <Image
+                className={styles.center}
+                width={490}
+                height={245}
+                src={whereIs}
+                alt='where is the profile'
+              />
+            </div>
+            <div className={styles.div}>
+              <p className={styles.p}>
+                Here is the <span className={styles.stats}>ID</span> that you
+                need
+              </p>
+              <Image
+                className={styles.center}
+                width={490}
+                height={245}
+                src={profile}
+                alt='where is the profile'
+              />
+            </div>
+          </article>
+        )}
+      </section>
+      <section>
+        <div onClick={() => setDropClub(!dropClub)} className={styles.dropClub}>
+          <h3 className={styles.text}>
+            <div className={`${dropClub && styles.open} ${styles.close}`}>
+              {'>'}
+            </div>{' '}
+            Where is my Club ID?
+          </h3>
+        </div>
+        {dropClub && (
+          <article className={styles.tutorial}>
+            <div className={styles.div}>
+              <p className={styles.p}>
+                Enter to your profile <span className={styles.stats}>here</span>
+              </p>
+              <Image
+                className={styles.center}
+                width={490}
+                height={245}
+                src={whereIs}
+                alt='where is the profile'
+              />
+            </div>
+            <div className={styles.div}>
+              <p className={styles.p}>
+                Here is the <span className={styles.stats}>ID</span> that you
+                need
+              </p>
+              <Image
+                className={styles.center}
+                width={490}
+                height={245}
+                src={profile}
+                alt='where is the profile'
+              />
+            </div>
+          </article>
+        )}
+      </section>
     </main>
   )
 }
