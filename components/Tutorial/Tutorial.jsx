21bb77dcde557dc89defa367a471c53c0f8c16a8
Facug03 <facundogonzales1550@gmail.com>
2023-01-06 16:56:25 -0300

    tutorial component and styles

---
+++ components/Tutorial/Tutorial.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+import styles from './Tutorial.module.css'
+
+export default function Tutorial({ drop, setDrop, type, children }) {
+  return (
+    <section>
+      <div
+        onClick={() => setDrop(!drop)}
+        className={`${type === 'Player' ? styles.drop : styles.dropClub}`}
+      >
+        <h3 className={styles.text}>
+          <div className={`${drop && styles.open} ${styles.close}`}>{'>'}</div>
+          Where is my {type} ID?
+        </h3>
+      </div>
+      {drop && <article className={styles.tutorial}>{children}</article>}
+    </section>
+  )
+}
