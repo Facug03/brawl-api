a9777970cd72e7c0f82860c3addd658669511947
Facug03 <facundogonzales1550@gmail.com>
2023-01-06 16:54:30 -0300

    separate search card in a component

---
+++ components/Card/Card.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
+import styles from './Card.module.css'
+
+export default function Card({ type }) {
+  const [id, setId] = useState('')
+
+  const controlInput = ({ target }) => {
+    if (target.value.length < 10 && /^[A-Za-z0-9]*$/.test(target.value)) {
+      setId(target.value.toUpperCase())
+    }
+  }
+
+  return (
+    <article className={styles.card}>
+      <h2 className={styles.search}>{type}</h2>
+      <form className={styles.form}>
+        <span className={styles.hashtag}>#</span>
+        <input
+          value={id}
+          name={type}
+          onChange={controlInput}
+          className={styles.tag}
+        />
+        <Link href={`/player/${id}`} className={styles.button}>
+          Search
+        </Link>
+      </form>
+    </article>
+  )
+}
