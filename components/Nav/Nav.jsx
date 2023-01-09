e8542aa463e3149e98563dfad9a576c92f89f376
Facug03 <facundogonzales1550@gmail.com>
2023-01-09 10:38:37 -0300

    improve nav styles

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import styles from './Nav.module.css'
 
 export default function Nav() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.game}>
-        Brawl<span className={styles.pro}>Pro</span>
-      </h1>
+      <Link href='/' className={styles.logo}>
+        <h1 className={styles.game}>
+          Brawl<span className={styles.pro}>Pro</span>
+        </h1>
+      </Link>
       <ul className={styles.nav}>
         <li>Eventos</li>
         <li>Rankings</li>
