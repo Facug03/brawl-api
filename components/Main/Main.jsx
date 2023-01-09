97faab73c84d274785baa747df136c5dca44c4f0
Facug03 <facundogonzales1550@gmail.com>
2023-01-09 19:13:33 -0300

    test

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -20,6 +20,9 @@ export default function Main() {
       {
         headers: {
           'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Methods': 'GET, OPTIONS',
+          'Access-Control-Max-Age': '86400',
         },
       }
     )
