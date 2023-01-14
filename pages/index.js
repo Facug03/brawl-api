0f10486f7b5bcca0797baca8bdfeb0841cce6f2c
Facug03 <facundogonzales1550@gmail.com>
2023-01-14 12:42:03 -0300

    text shadow style changed and add icon

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
           content='Brawl Pro. Brawl Stars Stats, information about Brawl Talk, best Brawlers for power league. Search your profile statistics, battle log, see leaderboard from the world and each country'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/crown.png' />
       </Head>
       <Main />
     </>
