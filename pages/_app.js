ff98162eb34398ed85970ae5cfff50a305922119
Facug03 <facundogonzales1550@gmail.com>
2023-01-16 16:25:20 -0300

    analytics vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Rubik } from '@next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 import Nav from '../components/Nav/Nav'
 import '../styles/globals.css'
@@ -7,9 +8,12 @@ const rubik = Rubik({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={rubik.className}>
-      <Nav />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className={rubik.className}>
+        <Nav />
+        <Component {...pageProps} />
+      </div>
+      <Analytics />
+    </>
   )
 }
