de460b2522431b2965811f1e60b5d446fe7c51d8
Facug03 <facundogonzales1550@gmail.com>
2023-01-17 20:51:06 -0300

    stats of victories brawlers

--- utils/postBrawlers.js
+++ utils/postBrawlers.js
@@ -14,41 +14,89 @@ export async function postBrawler(brawlers) {
 }
 
 export function sortBrawlers(items, player) {
-  const maps = items.filter(
+  const soloRanked = items.filter(
     (item) => item.battle.type === 'soloRanked' && item.battle.starPlayer
   )
 
-  let brawlersVictory = []
-  let brawlersDefeat = []
+  const showdown = items.filter((item) => item.battle.mode === 'soloShowdown')
 
-  maps.forEach((map) => {
+  let brawlersVictory = {}
+  let brawlersDefeat = {}
+
+  soloRanked.forEach((map) => {
     if (map.battle.teams[0].some((ele) => ele.name === player)) {
       const team0 = map.battle.teams[0].map((team) => team.brawler.name)
       const team1 = map.battle.teams[1].map((team) => team.brawler.name)
 
       if (map.battle.result === 'victory') {
-        brawlersVictory = [...brawlersVictory, ...team0]
-        brawlersDefeat = [...brawlersDefeat, ...team1]
+        brawlersVictory = {
+          ...brawlersVictory,
+          [map.event.id]: [...(brawlersVictory[map.event.id] || []), ...team0],
+        }
+        brawlersDefeat = {
+          ...brawlersDefeat,
+          [map.event.id]: [...(brawlersDefeat[map.event.id] || []), ...team1],
+        }
       } else {
-        brawlersVictory = [...brawlersVictory, ...team1]
-        brawlersDefeat = [...brawlersDefeat, ...team0]
+        brawlersVictory = {
+          ...brawlersVictory,
+          [map.event.id]: [...(brawlersVictory[map.event.id] || []), ...team1],
+        }
+        brawlersDefeat = {
+          ...brawlersDefeat,
+          [map.event.id]: [...(brawlersDefeat[map.event.id] || []), ...team0],
+        }
       }
     } else {
       const team0 = map.battle.teams[0].map((team) => team.brawler.name)
       const team1 = map.battle.teams[1].map((team) => team.brawler.name)
 
       if (map.battle.result === 'victory') {
-        brawlersVictory = [...brawlersVictory, ...team1]
-        brawlersDefeat = [...brawlersDefeat, ...team0]
+        brawlersVictory = {
+          ...brawlersVictory,
+          [map.event.id]: [...(brawlersVictory[map.event.id] || []), ...team1],
+        }
+        brawlersDefeat = {
+          ...brawlersDefeat,
+          [map.event.id]: [...(brawlersDefeat[map.event.id] || []), ...team0],
+        }
       } else {
-        brawlersVictory = [...brawlersVictory, ...team0]
-        brawlersDefeat = [...brawlersDefeat, ...team1]
+        brawlersVictory = {
+          ...brawlersVictory,
+          [map.event.id]: [...(brawlersVictory[map.event.id] || []), ...team0],
+        }
+        brawlersDefeat = {
+          ...brawlersDefeat,
+          [map.event.id]: [...(brawlersDefeat[map.event.id] || []), ...team1],
+        }
       }
     }
   })
 
-  return {
-    brawlersVictory,
-    brawlersDefeat,
+  showdown.forEach((map) => {
+    const winners = map.battle.players
+      .slice(0, 4)
+      .map((player) => player.brawler.name)
+    const losers = map.battle.players
+      .slice(4)
+      .map((player) => player.brawler.name)
+
+    brawlersVictory = {
+      ...brawlersVictory,
+      [map.event.id]: [...(brawlersVictory[map.event.id] || []), ...winners],
+    }
+    brawlersDefeat = {
+      ...brawlersDefeat,
+      [map.event.id]: [...(brawlersDefeat[map.event.id] || []), ...losers],
+    }
+  })
+
+  if (Object.keys(brawlersVictory).length) {
+    return {
+      brawlersVictory,
+      brawlersDefeat,
+    }
   }
+
+  return null
 }
