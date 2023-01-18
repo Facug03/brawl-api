b303f7b6a1ff97e70f24c93ccf1f4bf17134d97c
Facug03 <facundogonzales1550@gmail.com>
2023-01-17 21:58:06 -0300

    error

--- pages/api/rate.js
+++ pages/api/rate.js
@@ -1,7 +1,11 @@
 import { postBrawlers } from '../../lib/redis'
 
 export default async function handler(req, res) {
-  const brawlers = await postBrawlers(req.body)
+  try {
+    const brawlers = await postBrawlers(req.body)
 
-  res.status(200).json(brawlers)
+    res.status(200).json(brawlers)
+  } catch (error) {
+    res.status(200).json(err)
+  }
 }
