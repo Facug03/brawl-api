7a46a041b67c074cd561a3a13e8c668e22aee341
Facug03 <facundogonzales1550@gmail.com>
2023-01-20 23:07:26 -0300

    seo

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Brawl Pro | Brawl Stars Stats</title>
-        <meta name='Brawl pro' content='Brawl Pro' />
+        <meta name='application-name' content='Brawl pro' />
         <meta
           name='description'
           content='Brawl Pro. Brawl Stars Stats, information about Brawl Talk, best Brawlers for power league and showdown. Search your profile statistics, battle log, see leaderboard from the world and each country'
