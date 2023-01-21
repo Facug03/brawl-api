719afcfd53a23c4201277eb3b3f7350718344e0f
Facug03 <facundogonzales1550@gmail.com>
2023-01-20 23:35:06 -0300

    seo and manifest

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Brawl Pro | Brawl Stars Stats</title>
-        <meta name='application-name' content='Brawl pro' />
+        <meta name='application-name' content='Brawl Pro' />
         <meta
           name='description'
           content='Brawl Pro. Brawl Stars Stats, information about Brawl Talk, best Brawlers for power league and showdown. Search your profile statistics, battle log, see leaderboard from the world and each country'
         />
+        <meta name='apple-mobile-web-app-title' content='Brawl Pro' />
+        <meta property='og:title' content='Brawl Pro | Brawl Stars Stats' />
+        <meta property='og:url' content='https://www.brawlpro.com' />
+        <meta property='og:site_name' content='Brawl Pro' />
+        <meta property='og:type' content='website' />
+        <meta name='theme-color' content='#363b4e' />
         <meta name='google' content='notranslate' />
         <link rel='icon' href='/crown.png' />
+        <link rel='manifest' href='/manifest.json' />
       </Head>
       <Main />
     </>
