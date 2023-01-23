333faa5def1eccef9835028e57a1ed529dd64c28
Facundo Gonzalez <107658697+Facug03@users.noreply.github.com>
2023-01-23 12:39:22 -0300

    Test finished

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.experiments = config.experiments || {}
-    config.experiments.topLevelAwait = true
-    return config
-  },
   images: {
     domains: ['imagedelivery.net'],
   },
