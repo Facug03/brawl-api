416d1db48f17378b3abbc051d0a3e6bdf218a754
Facundo Gonzalez <107658697+Facug03@users.noreply.github.com>
2023-01-23 12:18:13 -0300

    Top level await

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-  config.experiments = {
+  experiments = {
     topLevelAwait: true,
   },
   images: {
