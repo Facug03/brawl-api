97da760b729e77010f49c7b2faed6c523ea46fe6
Facug03 <facundogonzales1550@gmail.com>
2023-01-27 18:22:19 -0300

    add footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Container from '../Container/Container'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <Container>
+      <footer>
+        <p>If you want to suggest any idea of send me some feedback:</p>
+        <p></p>
+      </footer>
+    </Container>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react'
 import Script from 'next/script'
 
 import Nav from '../components/Nav/Nav'
+import Footer from '../components/Footer/Footer'
 import '../styles/globals.css'
 
 const rubik = Rubik({ subsets: ['latin'] })
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }) {
       <div className={rubik.className}>
         <Nav />
         <Component {...pageProps} />
+        {/* <Footer /> */}
       </div>
       <Analytics />
     </>
