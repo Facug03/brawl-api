e27e73a87e296ef8e312882a9896a9699469e8e3
Facug03 <facundogonzales1550@gmail.com>
2023-01-26 22:39:48 -0300

    ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { Rubik } from '@next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 import Nav from '../components/Nav/Nav'
 import '../styles/globals.css'
@@ -9,6 +10,11 @@ const rubik = Rubik({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async={true}
+        crossOrigin='anonymous'
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8963046990416483'
+      />
       <div className={rubik.className}>
         <Nav />
         <Component {...pageProps} />
