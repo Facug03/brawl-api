51f64b417d593dc5f431ef8f493aaf74ca4ad2a1
Facug03 <facundogonzales1550@gmail.com>
2023-01-31 09:45:09 -0300

    add urls

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,121 @@ module.exports = {
   siteUrl: 'https://brawlpro.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
+  additionalPaths: async (config) => {
+    const result = []
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/P0PULRC'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/VLQPVPY'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/QRUQQLV0'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/LJCL8P0'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/82RGU8PR'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/YQUCCJ2'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/28PU0P9L0'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/8Y98Q8U'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/9ULYPV8'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/2282LR0YG'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/YJ88Q2G'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/80QUGYC'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/8PJRRG2C'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/PCPRPJV'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/8LQ9JR82'
+      )
+    )
+
+    result.push(
+      await config.transform(
+        { ...config, changefreq: 'hourly', priority: 0.6 },
+        '/player/QCC0UVC'
+      )
+    )
+
+    return result
+  },
 }
