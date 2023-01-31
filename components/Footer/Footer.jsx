73e38c23fd6cfbc9360f6e9d4fe503130214e027
Facug03 <facundogonzales1550@gmail.com>
2023-01-31 11:19:30 -0300

    add contact

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,7 +6,16 @@ export default function Footer() {
     <Container>
       <footer>
         <p>If you want to suggest any idea of send me some feedback:</p>
-        <p></p>
+        <p>brawlprocontact@gmail.com</p>
+        <p>
+          This content is not affiliated with, endorsed, sponsored, or
+          specifically approved by Supercell and Supercell is not responsible
+          for it. For more information see{' '}
+          <a href='https://supercell.com/en/fan-content-policy/'>
+            Supercell’s Fan Content Policy
+          </a>
+          .
+        </p>
       </footer>
     </Container>
   )
