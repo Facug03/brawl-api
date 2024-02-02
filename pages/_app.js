4d4b02e583d2d8a8fe09a97fe622262d2a8e5886
Facu <facundo.gonzalez@farmaloop.fc>
2024-02-02 12:32:19 -0300

    add ads

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { Rubik } from '@next/font/google'
 import Speed from '@components/Speed'
 import { Analytics } from '@vercel/analytics/react'
 // import { SpeedInsights } from '@vercel/speed-insights/next'
-// import Script from 'next/script'
+import Script from 'next/script'
 
 import Nav from '@components/Nav/Nav'
 import Footer from '@components/Footer/Footer'
@@ -13,16 +13,16 @@ const rubik = Rubik({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <Script
-        id='Adsense-id'
-        async='true'
-        strategy='afterInteractive'
-        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8963046990416483'
-        crossOrigin='anonymous'
+      <Script
+        id="Adsense-id"
+        async="true"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8963046990416483"
+        crossOrigin="anonymous"
         onError={(e) => {
           console.error('Script failed to load', e)
         }}
-      /> */}
+      />
       <div className={rubik.className}>
         <Nav />
         <Component {...pageProps} />
