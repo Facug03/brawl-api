6d015dcb5bbf2e0629c310591256cb182c1b1e7a
Facug03 <facundogonzales1550@gmail.com>
2024-05-10 11:56:57 -0300

    remove challengue title

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -10,7 +10,7 @@ export function Events({ events }) {
     <Container>
       <h1 className={styles.title}>Event Rotation Brawl Stars</h1>
       <section className={styles.section}>
-        {events.slice(0, 8).map((ele) => {
+        {events.map((ele) => {
           return (
             <article className={styles.article} key={ele.event.id}>
               <div style={{ background: mode[ele.event.mode]?.color }} className={styles.div}>
@@ -47,49 +47,6 @@ export function Events({ events }) {
           )
         })}
       </section>
-      {events.length > 8 && (
-        <>
-          <h2 className={styles.title}>Challenge</h2>
-          <section className={styles.section}>
-            {events.slice(8).map((ele) => {
-              return (
-                <article className={styles.article} key={ele.event.id}>
-                  <div style={{ background: mode[ele.event.mode]?.color }} className={styles.div}>
-                    <div className={styles.info}>
-                      <div className={styles.modeContainer}>
-                        <Image
-                          src={`https://imagedelivery.net/YuuZ9BLOxw-yqfwDx251Sg/${ele.event.mode}/mini`}
-                          alt={`${mode[ele.event.mode]} icon map brawl stars`}
-                          width={33}
-                          height={35}
-                          unoptimized={true}
-                        />
-                        <h3 className={styles.mode}>{mode[ele.event.mode]?.name}</h3>
-                      </div>
-                      <h4 className={styles.map}>{ele.event.map}</h4>
-                      <h5 className={styles.newMap}>{getInterval(ele.startTime, ele.endTime)}</h5>
-                    </div>
-                  </div>
-                  <Image
-                    className={`${ele.event.mode.toLowerCase().includes('showdown') && styles.showdown} ${styles.img} ${
-                      (ele.event.mode === 'roboRumble' ||
-                        ele.event.mode === 'basketBrawl' ||
-                        ele.event.mode === 'bossFight' ||
-                        ele.event.mode === 'volleyBrawl') &&
-                      styles.showdown
-                    } ${styles.img}`}
-                    src={`https://imagedelivery.net/YuuZ9BLOxw-yqfwDx251Sg/${ele.event.id}/map`}
-                    alt={`${ele.event.map}`}
-                    width={265}
-                    height={415}
-                    unoptimized={true}
-                  />
-                </article>
-              )
-            })}
-          </section>
-        </>
-      )}
     </Container>
   )
 }
